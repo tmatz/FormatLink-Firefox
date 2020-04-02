375b9ccb05d6f493cef8677a75abba77d7fe2333
MATSUDA Takashi <matsuda@hallab.co.jp>
2020-04-03 00:17:55 +0900

    feat: add swap button

--- options.js
+++ options.js
@@ -48,6 +48,23 @@ async function restoreDefaults() {
   return saveOptions(DEFAULT_OPTIONS["defaultFormat"]);
 }
 
+async function swapFormats(e, delta) {
+  const i1 = Number(e.target.id.replace(/^[a-z]+/, ""));
+  const i2 = i1 + delta;
+  if (1 <= i1 && i1 <= 9 && 1 <= i2 && i2 <= 9) {
+    const title1 = document.getElementById("title" + i1);
+    const format1 = document.getElementById("format" + i1);
+    const title2 = document.getElementById("title" + i2);
+    const format2 = document.getElementById("format" + i2);
+    let tmp = title1.value;
+    title1.value = title2.value;
+    title2.value = tmp;
+    tmp = format1.value;
+    format1.value = format2.value;
+    format2.value = tmp;
+  }
+}
+
 async function init() {
   await restoreOptions();
   document.getElementById("saveButton").addEventListener("click", function(e) {
@@ -60,5 +77,11 @@ async function init() {
       e.preventDefault();
       restoreDefaults();
     });
+  for (let i = 1; i <= 8; i++) {
+    document.getElementById("swap" + i)?.addEventListener("click", function(e) {
+      e.preventDefault();
+      swapFormats(e, 1);
+    });
+  }
 }
 document.addEventListener("DOMContentLoaded", init);
